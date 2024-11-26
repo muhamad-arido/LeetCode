49ffe88b02a4f5e15c9db748f3781ac624ec5e69
Arido <mhd.arido@gmail.com>
2024-11-26 15:04:35 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -0,0 +1,24 @@
+function isValid(s: string): boolean {
+  const isOpening = (value: string) => "([{".includes(value);
+  const isPair = (value: string) =>
+    value === "()" || value === "[]" || value === "{}";
+
+  if (s.length % 2) return false;
+  if ([...s].every((value) => isOpening(value))) return false;
+
+  const res: Array<string> = [];
+
+  const valid: Array<boolean> = [...s].map((value) => {
+    if (isOpening(value)) {
+      res.push(value);
+      return true;
+    }
+    if (isPair(res.at(-1) + value)) {
+      res.pop();
+      return true;
+    }
+    return false;
+  });
+
+  return valid.every((value) => value);
+}
