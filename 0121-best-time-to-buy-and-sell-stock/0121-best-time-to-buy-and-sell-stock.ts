daddfa769f6516a35453756a2fc1a44c31960ad7
Arido <mhd.arido@gmail.com>
2024-12-02 20:37:56 +0700

    Time: 6 ms (20.16%), Space: 61.9 MB (9.34%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,10 @@
 function maxProfit(prices: number[]): number {
   let minPrice: number = prices[0];
-  let profit: number[] = [];
 
-  prices.map((value: number) => {
+  const profit: number[] = prices.map((value: number) => {
     if (value < minPrice) minPrice = value;
-    if (value > minPrice) profit.push(value - minPrice);
+    return value - minPrice;
   });
 
-  return profit.length == 0 ? 0 : Math.max(...profit);
+  return Math.max(...profit);
 };
