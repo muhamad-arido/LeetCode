d47f6612caac4cb0d5121cf6cbb4efcdead93583
Arido <mhd.arido@gmail.com>
2024-12-02 19:11:18 +0700

    Time: 2 ms (22.18%), Space: 51.6 MB (63.46%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.ts
@@ -0,0 +1,18 @@
+function getRow(rowIndex: number): number[] {
+  const result: number[][] = [];
+
+  for (let i = 0; i <= rowIndex; i++) {
+    const row: number[] = [];
+
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        row.push(1);
+      } else {
+        const value: number = result[i - 1][j - 1] + result[i - 1][j];
+        row.push(value);
+      }
+    }
+    result.push(row);
+  }
+  return result[rowIndex];
+};
