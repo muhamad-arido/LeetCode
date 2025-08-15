9e06b56650dd4ba281446353775da453563b3751
Arido <mhd.arido@gmail.com>
2025-08-15 23:25:11 +0700

    Time: 1 ms (34.72%), Space: 57.9 MB (34.72%) - LeetHub

--- LeetCode/Easy/0342-power-of-four/0342-power-of-four.ts
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.ts
@@ -1,21 +1,9 @@
 function isPowerOfFour(n: number): boolean {
   if (n <= 0) return false;
 
-  let low = 1;
-  let high = n;
-
-  while (low <= high) {
-    let mid = (low + high) >> 1;
-    let power = mid * mid * mid * mid;
-
-    if (power === n) {
-      return true;
-    } else if (power < n) {
-      low = mid + 1;
-    } else {
-      high = mid - 1;
-    }
+  while (n % 4 === 0) {
+    n = n / 4;
   }
 
-  return false;
+  return n === 1;
 };
